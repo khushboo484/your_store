b0ac3d457cd6fa4b963d5586054c512e5230c790
Khushboo <112khushimeh@gmail.com>
2021-05-22 23:43:09 +0530

    web app

--- src/components/menuItems/index.jsx
+++ src/components/menuItems/index.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
-import Chinese from '../../images/thumbnails/chinese.png';
-import Indian from '../../images/thumbnails/desi.jpg';
-import Drink from '../../images/thumbnails/beverage.jpg';
-import Italian from '../../images/thumbnails/italian.png';
-import NONVEG from '../../images/thumbnails/non-veg.jpg';
-import FAST from '../../images/thumbnails/fast.jpg';
 import { deviceSize } from "../responsive";
 import { Marginer } from '../marginer';
 
+import Burger from "../../images/thumbnails/burgers.png";
+import Chicken from "../../images/thumbnails/chicken.jpg";
+import Cake from "../../images/thumbnails/cakes.png";
+import Dosa from "../../images/thumbnails/dosa.png";
+import Noodles from "../../images/thumbnails/noodles.png";
+import Pizza from "../../images/thumbnails/pizza.png";
+import Shakes from "../../images/thumbnails/shakes.png";
+import Tikka from "../../images/thumbnails/tikka.png";
+
 const ListContainer = styled.div`
+    margin-top: 10px;
     height: 100px;
     width: 100%;
     display: flex;
@@ -18,7 +22,9 @@ const ListContainer = styled.div`
 `;
 
 const ImgContainer = styled.div`
-    border: 2px solid yellow;
+    border-left: 3px solid #98FB98;
+    border-right: 3px solid #98FB98;
+    border-radius: 15px;
     margin: 10px;
     height: 100%;
     width: 100%;
@@ -53,34 +59,44 @@ export function MenuItems(props) {
     return (
         <ListContainer>
             <ImgContainer>
-            <Image size={sizeImg}><img src={Chinese} alt="Chin" /></Image>
-            <Marginer direction="vertical" size={6}/>
+            <Image size={sizeImg}><img src={ Noodles } alt="Chin" /></Image>
+            <Marginer direction="vertical" size={4}/>
             <LabelText>Chinese</LabelText>
             </ImgContainer>
             <ImgContainer>
-            <Image size={sizeImg}><img src={Indian} alt="Indn" /></Image>
-            <Marginer direction="vertical" size={6}/>
+            <Image size={sizeImg}><img src={ Dosa } alt="Indn" /></Image>
+            <Marginer direction="vertical" size={4}/>
             <LabelText>Desi Food</LabelText>
             </ImgContainer>
             <ImgContainer>
-            <Image size={sizeImg}><img src={Drink} alt="Bevg" /></Image>
-            <Marginer direction="vertical" size={6}/>
-            <LabelText>Beverages</LabelText>
+            <Image size={sizeImg}><img src={ Shakes } alt="Bevg" /></Image>
+            <Marginer direction="vertical" size={4}/>
+            <LabelText>Shakes</LabelText>
             </ImgContainer>
             <ImgContainer>
-            <Image size={sizeImg}><img src={FAST} alt="food" /></Image>
-            <Marginer direction="vertical" size={6}/>
+            <Image size={sizeImg}><img src={ Burger } alt="food" /></Image>
+            <Marginer direction="vertical" size={4}/>
             <LabelText>Fast Food</LabelText>
             </ImgContainer>
             <ImgContainer>
-            <Image size={sizeImg}><img src={Italian} alt="Ita" /></Image>
-            <Marginer direction="vertical" size={6}/>
+            <Image size={sizeImg}><img src={ Pizza } alt="Ita" /></Image>
+            <Marginer direction="vertical" size={4}/>
             <LabelText>Italian</LabelText>
             </ImgContainer>
             <ImgContainer>
-            <Image size={sizeImg}><img src={NONVEG} alt="non_veg" /></Image>
-            <Marginer direction="vertical" size={6}/>
-            <LabelText>Non Veg</LabelText>
+            <Image size={sizeImg}><img src={ Chicken } alt="non_veg" /></Image>
+            <Marginer direction="vertical" size={4}/>
+            <LabelText>Chicken</LabelText>
+            </ImgContainer>
+            <ImgContainer>
+            <Image size={sizeImg}><img src={ Tikka } alt="tikka" /></Image>
+            <Marginer direction="vertical" size={4}/>
+            <LabelText>Tikka Special</LabelText>
+            </ImgContainer>
+            <ImgContainer>
+            <Image size={sizeImg}><img src={ Cake } alt="cake" /></Image>
+            <Marginer direction="vertical" size={4}/>
+            <LabelText>Desserts</LabelText>
             </ImgContainer>
         </ListContainer>
     )
